7a79f1205c963076fd395058272897134dad7d05
FETE <starshine@aili.moe>
2021-03-31 09:55:08 +0800

    init repo

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import { Quasar } from 'quasar'
+import quasarUserOptions from './quasar.user.config'
+
+createApp(App).use(Quasar, quasarUserOptions).use(router).mount('#app')
---
+++ src/quasar.user.config.js
@@ -0,0 +1,15 @@
+
+import './styles/quasar.scss'
+import iconSet from 'quasar/icon-set/eva-icons.js'
+import lang from 'quasar/lang/zh-CN.js'
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/eva-icons/eva-icons.css'
+
+// To be used on app.use(Quasar, { ... })
+export default {
+  config: {},
+  plugins: {
+  },
+  lang: lang,
+  iconSet: iconSet
+}
